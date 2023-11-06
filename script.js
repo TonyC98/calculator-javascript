0a81f932a1459b74a5d4b36aea5b922d6868fc1d
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-11-06 16:13:17 +0000

    added calculator class and for each loop to update display numbers

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
+
+delete() {
+     
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+
+}
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll(['data-operation'])
 const equalsButton = document.querySelector(['data-equals'])
@@ -6,14 +42,11 @@ const allClearButton = document.querySelector(['data-all-clear'])
 const previousOperandTextElement = document.querySelector(['data-previous-operand'])
 const currentOperandTextElement = document.querySelector(['data-current-operand'])
 
-clear() {
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-}
-
-deleteLast() {
-     
-}
-
-appendNumber() {
-
-}
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
